3350cf275b80ee8c6f58e669aa49bd3bc03ae29f
Steven Passanante <xzmilan@gmail.com>
2015-01-25 18:37:40 -0500

    Edited cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##MY CODE FOR CACHING THE INVERSE OF A MATRIX AND 
+#FUNCTION TO CREATE MATRIX AND ITS INVERSE FOR A SQUARE 2X2
 makeCacheMatrix <- function(x = matrix()) {
-
+  Mtx<-NULL
+  MtxID <<- diag(nrow = 2, ncol = 2)
+    set<-function(y){
+    x <<- y
+    Mtx <<- NULL
+  }
+  get<- function() x 
+  setMtxInverse <-function(inverse) Mtx<<- inverse
+  getMtxInverse<-function() Mtx
+  list(set=set, get=get,
+       setMtxInverse=setMtxInverse,
+       getMtxInverse=getMtxInverse)
 }
-
-
-## Write a short comment describing this function
-
+#Retrieving the Matrix, and setting Inverse 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  Mtx<-x$getMtxInverse()
+  if(!is.null(Mtx)){
+    print("retrieving cached data")
+    return(Mtx)
+  }
+  
+  Mtx<-solve(x$get())
+  x$setMtxInverse(Mtx)
+  Mtx
 }
+
+#Setting Matrix variables, and calculating Inverse, and test against Identity Matrix
+a <- makeCacheMatrix(matrix(1:4,2))
+#Square Matrix  
+a$get()
+#Inverse of Square Martrix
+cacheSolve(a)
+#Test Against Identity Matrix 
+MtxID
+#Production of Identity Matrix from Current Square Matrix "*" Inverse 
+a$get()%*%a$getMtxInverse()
+
+
